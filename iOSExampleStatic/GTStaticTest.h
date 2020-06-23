843d102f4ca4626e33d9926c9c0224885a08625a
刘玉祥 <llliuyx@aliyun.com>
2020-06-23 18:42:38 +0800

    新增GTStaticTest

---
+++ iOSExampleStatic/GTStaticTest.h
@@ -0,0 +1,17 @@
+//
+//  GTStaticTest.h
+//  iOSExampleStatic
+//
+//  Created by liuyx on 2020/6/23.
+//  Copyright © 2020 liuyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GTStaticTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSExampleStatic/GTStaticTest.m
@@ -0,0 +1,13 @@
+//
+//  StaticTest.m
+//  iOSExampleStatic
+//
+//  Created by liuyx on 2020/6/23.
+//  Copyright © 2020 liuyx. All rights reserved.
+//
+
+#import "GTStaticTest.h"
+
+@implementation GTStaticTest
+
+@end
