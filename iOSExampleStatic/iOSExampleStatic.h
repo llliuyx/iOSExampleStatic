e424ca15f6442f5714b37eb7aa932b8c156c02f2
刘玉祥 <llliuyx@aliyun.com>
2020-06-23 18:34:37 +0800

    Initial Commit

---
+++ iOSExampleStatic/iOSExampleStatic.h
@@ -0,0 +1,13 @@
+//
+//  iOSExampleStatic.h
+//  iOSExampleStatic
+//
+//  Created by liuyx on 2020/6/23.
+//  Copyright © 2020 liuyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSExampleStatic : NSObject
+
+@end
---
+++ iOSExampleStatic/iOSExampleStatic.m
@@ -0,0 +1,13 @@
+//
+//  iOSExampleStatic.m
+//  iOSExampleStatic
+//
+//  Created by liuyx on 2020/6/23.
+//  Copyright © 2020 liuyx. All rights reserved.
+//
+
+#import "iOSExampleStatic.h"
+
+@implementation iOSExampleStatic
+
+@end
